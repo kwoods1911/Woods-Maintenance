3c57e3c93727d7d26b3138df4d31de2923d83876
Khari Woods <khariwoods3@gmail.com>
2018-09-09 14:38:50 -0400

    Added a contact page. Made use of google maps api. New js file added called 'maps,js'

---
+++ assets/js/map.js
@@ -0,0 +1,15 @@
+
+
+function initMap(){
+	var nassau = { lat:25.073096, lng:-77.3169152};
+
+	var map = new google.maps.Map(document.getElementById('map'),{
+		zoom:14,
+		center: nassau
+	});
+
+	var marker = new google.maps.Marker({
+		position: nassau,
+		map: map
+	})
+}
